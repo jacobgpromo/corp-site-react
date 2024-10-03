f64ab97d6f5fe52d37bea7a231cd0b59aa6808cf
Jacob Goodall <JacobG@overturepromo.com>
2024-10-03 11:50:09 -0500

    jobs

--- client/src/components/Careers/Jobs.js
+++ client/src/components/Careers/Jobs.js
@@ -51,17 +51,6 @@ class Jobs extends Component {
               <a
                 className="apply-link"
                 target="blank"
-                href="https://overturepromotions.applytojob.com/apply/V3lzXUzbRP/Customer-Success-Manager"
-              >
-                Customer Success Manager
-              </a>
-            </h3>
-          </div>
-          <div style={{ borderTop: "1px solid #d8dadc" }}>
-            <h3>
-              <a
-                className="apply-link"
-                target="blank"
                 href="https://overturepromotions.applytojob.com/apply/7NCCz6Sszg/Training-Coordinator"
               >
                 Training Coordinator
