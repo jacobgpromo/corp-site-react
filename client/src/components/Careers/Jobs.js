9a3408102d299745113eb2de1ebf9970bfd7d71a
Jacob Goodall <JacobG@overturepromo.com>
2024-11-13 15:12:44 -0600

    jobs

--- client/src/components/Careers/Jobs.js
+++ client/src/components/Careers/Jobs.js
@@ -57,6 +57,17 @@ class Jobs extends Component {
               </a>
             </h3>
           </div>
+          <div style={{ borderTop: "1px solid #d8dadc" }}>
+            <h3>
+              <a
+                className="apply-link"
+                target="blank"
+                href="https://overturepromotions.applytojob.com/apply/X831dykukV/ECommerce-Manager"
+              >
+                E-Commerce Manager
+              </a>
+            </h3>
+          </div>
 
           {/* <div style={{borderTop: '1px solid #d8dadc'}}>
                         <h3>Web Developer </h3>
