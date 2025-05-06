4f9a2a95bef9ff4b98161c81febc372834221588
Jacob Goodall <JacobG@overturepromo.com>
2025-05-06 10:42:26 -0500

    jobs

--- client/src/components/Careers/Jobs.js
+++ client/src/components/Careers/Jobs.js
@@ -41,18 +41,6 @@ class Jobs extends Component {
               <a
                 className="apply-link"
                 target="blank"
-                href="https://overturepromotions.applytojob.com/apply/C6Iue4lqaM/Staff-Accountant"
-              >
-                Staff Accountant
-              </a>
-            </h3>
-            <p>Compensation: 60k-75k annually</p>
-          </div>
-          <div style={{ borderTop: "1px solid #d8dadc" }}>
-            <h3>
-              <a
-                className="apply-link"
-                target="blank"
                 href="https://overturepromotions.applytojob.com/apply/h0Xe1FhAnc/Account-Manager"
               >
                 Account Manager
